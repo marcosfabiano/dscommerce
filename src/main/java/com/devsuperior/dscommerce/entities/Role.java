aff7fc5ba93221d6dda180a2cecc6eb71b45724c
Marcos Fabiano <marcos.fabiano67@outlook.com>
2022-09-06 16:32:08 -0300

    User Role model

---
+++ src/main/java/com/devsuperior/dscommerce/entities/Role.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dscommerce.entities;
+
+import javax.persistence.*;
+@Entity
+@Table(name = "tb_role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String authority;
+
+    public Role() {
+    }
+
+    public Role(Long id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+}
