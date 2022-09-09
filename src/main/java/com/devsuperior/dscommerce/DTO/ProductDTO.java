0036c35fbe45bbdd68b5ed18322430a6330189c7
Marcos Fabiano <marcos.fabiano67@outlook.com>
2022-09-09 11:32:13 -0300

    ProductDTO with categories

--- src/main/java/com/devsuperior/dscommerce/DTO/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/DTO/ProductDTO.java
@@ -1,7 +1,10 @@
 package com.devsuperior.dscommerce.DTO;
 
+import com.devsuperior.dscommerce.entities.Category;
 import com.devsuperior.dscommerce.entities.Product;
 import javax.validation.constraints.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductDTO {
 
@@ -15,6 +18,8 @@ public class ProductDTO {
     @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
+    @NotEmpty(message = "Deve ter pelo menos uma categoria")
+    private List<CategoryDTO> categories = new ArrayList<>();
 
     public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
         this.id = id;
@@ -30,6 +35,9 @@ public class ProductDTO {
         description = entity.getDescription();
         price = entity.getPrice();
         imgUrl = entity.getImgUrl();
+        for (Category cat : entity.getCategories()) {
+            categories.add(new CategoryDTO(cat));
+        }
     }
 
     public Long getId() {
@@ -50,5 +58,8 @@ public class ProductDTO {
 
     public String getImgUrl() {
         return imgUrl;
+    }
+    public List<CategoryDTO> getCategories() {
+        return categories;
     }
 }
